47dfe2aab6fe14e300368bd9e68c7ac0a50fa4ec
deysia02 <iblaszak@edu.cdv.pl>
2025-11-21 13:01:15 +0100

    Add files via upload

---
+++ Zadanie2/Program.cs
@@ -0,0 +1,13 @@
+﻿int number;
+
+do
+{
+    Console.Write("Podaj liczbę wiekszą od 0: ");
+    number = int.Parse(Console.ReadLine());
+    if (number == 0 || number < 0)
+    {
+        Console.WriteLine("Zła liczba, jeszcze raz");
+    }
+}
+while (number == 0 || number < 0);
+Console.WriteLine("To poprawna liczba, gratulacje");
