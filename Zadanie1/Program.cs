24fe96049993972356af8a8f2f8606fc0e307ede
deysia02 <iblaszak@edu.cdv.pl>
2025-11-21 12:57:42 +0100

    Add files via upload

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,16 @@
+﻿string password = "admin123";
+string input = "";
+
+Console.WriteLine("Podaj hasło:");
+
+while (input != password)
+{
+    input = Console.ReadLine();
+    if (input != password)
+    {
+        Console.WriteLine("Złe hasło, spróbuj ponownie");
+    }    else
+    {
+        Console.WriteLine("Hasło poprawne, możesz wejśc");
+    }
+}
