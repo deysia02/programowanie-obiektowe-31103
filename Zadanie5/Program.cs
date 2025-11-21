70de1bfcae66419fe328df07610175f715e58498
deysia02 <iblaszak@edu.cdv.pl>
2025-11-21 13:06:50 +0100

    Add files via upload

---
+++ Zadanie5/Program.cs
@@ -0,0 +1,33 @@
+﻿class KontoBankowe
+{
+    private double saldo;
+    public void Wplata(double kwota) {saldo += kwota;}
+    public double PobierzSaldo() {return saldo;}
+    public void Wyplata(double kwota)
+    {
+        if (kwota <= saldo)
+        {
+            saldo -= kwota;
+            Console.WriteLine($"Wypłacono {kwota}, nowe saldo: {saldo}");
+        }
+        else
+        {
+            Console.WriteLine("Nie można wypłacić - niewystarczające saldo");
+        }
+    }
+}
+
+class Start
+{
+    static void Main()
+    {
+        KontoBankowe konto = new KontoBankowe();
+        konto.Wplata(1000);
+        Console.WriteLine($"Saldo po wpłacie: {konto.PobierzSaldo()}");
+
+        konto.Wyplata(500);
+        konto.Wyplata(500);
+        Console.WriteLine($"Saldo: {konto.PobierzSaldo()}");
+        konto.Wyplata(500);
+    }
+}
